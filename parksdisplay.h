a1fc614a51aca4965cd58abfb1be357d19f395f4
Newsha-N <70183115+Newsha-N@users.noreply.github.com>
2020-10-18 23:00:21 -0700

    Update parksdisplay.h

--- parksdisplay.h
+++ parksdisplay.h
@@ -18,20 +18,63 @@ class ParksDisplay : public QDialog
     Q_OBJECT
 
 public:
+        /****************************************************************
+		 * ParksDisplay ();
+		 *   Constructor; initalizes the paratmersx
+		 *   Parameters: parent (QWidget) IN - input file name
+		 *   Return: none
+		 ***************************************************************/
     explicit ParksDisplay(QWidget *parent = nullptr);
+    
+        /****************************************************************
+		 * ~ParksDisplay ();
+		 *   Destructor; 
+		 *   Parameters: none
+		 *   Return: none
+		 ***************************************************************/
     ~ParksDisplay();
 
 private slots:
+    /****************************************************************
+     * on_nameButton_clicked ();
+     *   member; sorts by the name of the parks
+     ***************************************************************/
     void on_nameButton_clicked();
-
+    
+    /****************************************************************
+     * on_resetButton_clicked ();
+     *   member; resets the table to the first organization
+     ***************************************************************/
     void on_resetButton_clicked();
 
+    /****************************************************************
+     * on_SortByArea_clicked ();
+     *   member; sorts the parks by area
+     ***************************************************************/
     void on_SortByArea_clicked();
+    
+     /****************************************************************
+     * on_SortByV_clicked ();
+     *   member; sorts the parks by the number of visitors
+     ***************************************************************/
     void on_SortByV_clicked();
+    
+     /****************************************************************
+     * on_parkNameButton_clicked ();
+     *   member; 
+     ***************************************************************/
     void on_parkNameButton_clicked();
 
+    /****************************************************************
+     * on_pushButton_clicked ();
+     *   member; 
+     ***************************************************************/
     void on_pushButton_clicked();
 
+    /****************************************************************
+     * on_pushButton_2_clicked ();
+     *   member;
+     ***************************************************************/
     void on_pushButton_2_clicked();
 
 
