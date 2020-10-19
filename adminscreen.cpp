c949acc65917805ee8f7d7743ca581b2e7c6e214
Max K <58583254+MaterialMacer@users.noreply.github.com>
2020-10-19 14:04:14 -0700

    Add files via upload

--- adminscreen.cpp
+++ adminscreen.cpp
@@ -1,5 +1,8 @@
 #include "adminscreen.h"
 #include "ui_adminscreen.h"
+#include "texteditor.h"
+#include "databasesetup.h"
+#include <iostream>
 
 AdminScreen::AdminScreen(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +14,32 @@ AdminScreen::AdminScreen(QWidget *parent) :
 AdminScreen::~AdminScreen()
 {
     delete ui;
+}
+
+void AdminScreen::on_contactInfoEditGo_clicked()
+{
+    mTexteditor = new texteditor(this);
+
+    mTexteditor->show();
+}
+
+void AdminScreen::on_addPark_clicked(int id, QString park_name, QString state, int acres)
+{
+    {
+        QSqlQuery query2;
+
+        query2.prepare("INSERT INTO customers (id, park_name, state, visitor, acres) VALUES (?,?,?,?,?);");
+
+        QTextStream in(stdin);
+
+        std::cin >> id;
+        in >> park_name;
+        in >> state;
+        std::cin >> acres;
+
+        query2.addBindValue(id);
+        query2.addBindValue(park_name);
+        query2.addBindValue(state);
+        query2.addBindValue(acres);
+    }
 }
