a95eb3c1147c1a415df2747c2a83fec286735d41
Max K <58583254+MaterialMacer@users.noreply.github.com>
2020-10-19 14:07:22 -0700

    Add files via upload

--- adminscreen.h
+++ adminscreen.h
@@ -2,6 +2,7 @@
 #define ADMINSCREEN_H
 
 #include <QDialog>
+#include "texteditor.h"
 
 namespace Ui {
 class AdminScreen;
@@ -15,8 +16,14 @@ public:
     explicit AdminScreen(QWidget *parent = nullptr);
     ~AdminScreen();
 
+private slots:
+    void on_contactInfoEditGo_clicked();
+    
+    void on_addPark_clicked(int id, QString park_name, QString state, int acres);
+
 private:
     Ui::AdminScreen *ui;
+    texteditor* mTexteditor;
 };
 
 #endif // ADMINSCREEN_H
