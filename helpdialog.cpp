3425a2087c967afced6ba64c8ea840ce2c95922b
Newsha-N <70183115+Newsha-N@users.noreply.github.com>
2020-10-18 22:26:55 -0700

    Update helpdialog.cpp

--- helpdialog.cpp
+++ helpdialog.cpp
@@ -1,13 +1,21 @@
 #include "helpdialog.h"
 #include "ui_helpdialog.h"
 
+/****************************************************************
+ * Default Constructor
+ *   This method sets up the help button and helps the costumer 
+ *   to find information easier
+ *       - returns  nothing  This will output the help dialoge.
+ ***************************************************************/
 helpDialog::helpDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::helpDialog)
 {
     ui->setupUi(this);
 }
-
+/****************************************************************
+ * Default Destructor
+ */
 helpDialog::~helpDialog()
 {
     delete ui;
